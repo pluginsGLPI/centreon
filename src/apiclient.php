f9b1568c5e2f766aeed5dbae2236b7a018697a03
Mary-Clb <mbressy@teclib.com>
2022-12-22 14:31:30 +0100

    get requests

--- src/apiclient.php
+++ src/apiclient.php
@@ -7,57 +7,75 @@ require 'environnement.php';
 
 use Glpi\Application\ErrorHandler;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
 
 class ApiClient
 {
+    private $auth_token = '';
 
-//Connexion to CENTREON API
-    public function CentronApiConnexion()
+    //Connection to CENTREON API
+    public function ConnectionRequest()
     {
-        $api_client = new Client([
-            'base_uri'  => CENTREON_URL,
-            'verify'    => false,
-        ]);
-    
-    try {
+        $api_client = new Client(['base_uri' =>  CENTREON_URL]);
 
-        $response = $api_client->request('POST', 'login', [
-            'headers'   => [
-                'Content-Type'  =>  "application/json",
-            ],
-            'json'      => [
-                'security'  => [
-                    'credentials'   => [
-                        'login'     =>  API_USER,
-                        'password'  =>  API_PASSWORD,
+        try {
+            $response    =  $api_client->request('POST', 'login', [
+                'headers' => [
+                    'Content-Type' => "application/json",
+                ],
+                'json' => [
+                    'security'  => [
+                        'credentials' => [
+                            'login' => API_USER,
+                            'password' => API_PASSWORD,
+                        ]
                     ]
                 ]
-            ]
-        ]);
+            ]);
+        } catch (\Exception $e) {
+            ErrorHandler::getInstance()->handleException($e, true);
+            return false;
+        }
 
-        $response_body  = $response->getBody();
-        $response_exp   = json_decode($response_body, true);
-        $auth_token     = $response_exp["security"]["token"];
-        $response_status= $response->getStatusCode();
+        $response_code      = $response->getStatusCode();
+        $response_body      = $response->getBody();
+        $response_data      = json_decode($response_body, true);
+        $this->auth_token   = $response_data["security"]["token"];
 
-        \Session::addMessageAfterRedirect(
-            __("Vous êtes connecté à l'API Centreon !")
-        );        
-    
-        } 
-        catch (\Exception $e) 
-        {
-            return $response_status;
-            return $response_exp["message"];
-            \Session::addMessageAfterRedirect(
-                __("Une erreur s'est produite")
-            );
-        } 
-
-
-
-        
+        return $response_data;
     }
 
+    protected function clientRequest($method, $endpoint = '', $params = [])
+    {
+        $api_client        = new Client(['base_uri' =>  CENTREON_URL]);
+        $params['headers'] = ['Content-Type' => "application/json", 'X-AUTH-TOKEN' => $this->auth_token];
+
+        try {
+            $response   = $api_client->request($method, $endpoint, $params);
+        } catch (\Exception $e) {
+            ErrorHandler::getInstance()->handleException($e, true);
+            return false;
+        }
+
+        $data_body = $response->getBody();
+        $data      = json_decode($data_body, true);
+        return $data;
+    }
+
+    protected function getHostsList($method = 'GET', $endpoint = 'monitoring/hosts', $params = [])
+    {
+        $response   = $this->clientRequest($method, $endpoint, $params);
+        return $response;
+    }
+
+    protected function getServicesList($method = 'GET', $endpoint = 'monitoring/services', $params = []) 
+    {
+        $response   = $this->clientRequest($method, $endpoint, $params);
+        return $response;
+    }
+
+    /*protected function getServicesListForOneHost($method = 'GET', $endpoint = 'monitoring/services/'.$host_id.'services', $params = [])
+    {
+        $response   = $this->clientRequest($method, $endpoint, $params);
+        return $response;
+    }*/
 }
