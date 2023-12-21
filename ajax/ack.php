bd79212f78d3d33aef19ca966b58e59d9d445007
Cédric Anne <cedric.anne@gmail.com>
2023-12-21 11:34:00 +0100

    Update ajax/ack.php
    Co-authored-by: Alexandre Delaunay <delaunay.alexandre@gmail.com>

--- ajax/ack.php
+++ ajax/ack.php
@@ -36,7 +36,6 @@ Session::checkRight('computer', UPDATE);
 
 header("Content-Type: text/html; charset=UTF-8");
 
-
 if (isset($_POST['hostid'])) {
     $host   = new Host();
     $res_a  = $host->acknowledgement($_POST['hostid'], $_POST['params']);
