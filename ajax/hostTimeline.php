7cc3c7c3850af6c37f6e571cdb9ce236d6eee831
mbressy <bressy.marie@gmail.com>
2025-06-19 13:37:13 +0000

    fix downtimes and cancel

--- ajax/hostTimeline.php
+++ ajax/hostTimeline.php
@@ -33,6 +33,7 @@ use GlpiPlugin\Centreon\Host;
 
 include('../../../inc/includes.php');
 
+Session::checkLoginUser();
 Session::checkRight('computer', READ);
 
 header('Content-Type: text/html; charset=UTF-8');
