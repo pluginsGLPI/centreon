8a0d3dba820e4c05c67d75a95c63eec42914f106
mbressy <bressy.marie@gmail.com>
2025-06-24 09:16:32 +0000

    fix acknowledgement

--- ajax/ack.php
+++ ajax/ack.php
@@ -30,9 +30,6 @@
 
 use GlpiPlugin\Centreon\Host;
 
-include('../../../inc/includes.php');
-
-Session::checkLoginUser();
 Session::checkRight('computer', UPDATE);
 
 header('Content-Type: text/html; charset=UTF-8');
