ee2dedf4bad06aa3b2674c3f311fbad7b8b97efc
Romain B. <8530352+Rom1-B@users.noreply.github.com>
2025-11-25 09:48:44 +0100

    1.1.1 (#68)

--- setup.php
+++ setup.php
@@ -32,7 +32,7 @@ use GlpiPlugin\Centreon\Host;
 
 use function Safe\define;
 
-define('PLUGIN_CENTREON_VERSION', '1.1.0');
+define('PLUGIN_CENTREON_VERSION', '1.1.1');
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_CENTREON_MIN_GLPI_VERSION', '11.0.0');
